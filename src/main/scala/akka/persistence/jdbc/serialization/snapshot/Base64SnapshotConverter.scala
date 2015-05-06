e808c1ba401ef19de4a9dc112192a160441310fa
Michael Kohout <mwkohout@bestbuy.com>
2015-05-06 17:20:39 -0500

    uses the apache commons codec Base64 encoder/decoder, vs the java8-only Base64 encoder/decoder.

--- src/main/scala/akka/persistence/jdbc/serialization/snapshot/Base64SnapshotConverter.scala
+++ src/main/scala/akka/persistence/jdbc/serialization/snapshot/Base64SnapshotConverter.scala
@@ -1,15 +1,15 @@
 package akka.persistence.jdbc.serialization.snapshot
 
-import java.util.Base64
 
 import akka.persistence.jdbc.serialization.SnapshotTypeConverter
 import akka.persistence.serialization.Snapshot
 import akka.serialization.Serialization
+import org.apache.commons.codec.binary.Base64
 
 class Base64SnapshotConverter extends SnapshotTypeConverter {
    override def marshal(value: Snapshot)(implicit serialization: Serialization): String =
-     serialization.serialize(value).map(Base64.getEncoder.encodeToString).get
+     serialization.serialize(value).map(new Base64().encodeToString).get
 
    override def unmarshal(value: String)(implicit serialization: Serialization): Snapshot =
-     serialization.deserialize(Base64.getDecoder.decode(value), classOf[Snapshot]).get
+     serialization.deserialize(new Base64().decode(value), classOf[Snapshot]).get
  }

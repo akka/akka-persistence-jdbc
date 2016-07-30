ed7f75a443f55408765b8199b21d82da4c620698
Dennis Vriend <dnvriend@gmail.com>
2016-07-30 11:12:29 +0200

    wait for services to be up

--- build.sh
+++ build.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
+export VM_HOST="${VM_HOST:-boot2docker}"
+
+# Wait for a certain service to become available
+# Usage: wait 3306 Mysql
+wait() {
+while true; do
+  if ! nc -z $VM_HOST $1
+  then
+    echo "$2 not available, retrying..."
+    sleep 1
+  else
+    echo "$2 is available"
+    break;
+  fi
+done;
+}
 rm ./bintray.sbt
+
+wait 3306 MySQL
+wait 1521 Oracle
+
 sbt test

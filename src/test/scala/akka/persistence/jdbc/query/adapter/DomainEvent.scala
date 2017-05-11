ed53ac0501f88ec466bb493ef7dba111cc388128
aenevala <aenevala@gmail.com>
2017-05-11 10:24:34 +0300

    Code style fix

--- src/test/scala/akka/persistence/jdbc/query/adapter/DomainEvent.scala
+++ src/test/scala/akka/persistence/jdbc/query/adapter/DomainEvent.scala
@@ -12,7 +12,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- */package akka.persistence.jdbc.query.adapter
+ */
+
+package akka.persistence.jdbc.query.adapter
 
 /**
   * Sample domain event used in [[akka.persistence.jdbc.query.EventAdapterTest]]

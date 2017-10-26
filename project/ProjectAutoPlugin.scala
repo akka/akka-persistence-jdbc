aa991a5e7f9060c6c42a0df220535c0439a3742a
Sean Sullivan <ssullivan@gilt.com>
2017-10-26 19:04:14 -0400

    Scala 2.12.4

--- project/ProjectAutoPlugin.scala
+++ project/ProjectAutoPlugin.scala
@@ -36,9 +36,9 @@ object ProjectAutoPlugin extends AutoPlugin {
 
     licenses += ("Apache-2.0", url("http://opensource.org/licenses/apache2.0.php")),
 
-    scalaVersion := "2.12.3",
+    scalaVersion := "2.12.4",
 
-    crossScalaVersions := Seq("2.11.11", "2.12.3"),
+    crossScalaVersions := Seq("2.11.11", "2.12.4"),
 
     fork in Test := true,
 

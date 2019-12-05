fd586a42631bfc8cf776db91a7d3e7e3059357ce
Marcos Pereira <marcos.silva@gmail.com>
2019-12-05 13:50:01 -0500

    Align Scala 2.13 version between build and travis

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   val Nightly = sys.env.get("TRAVIS_EVENT_TYPE").contains("cron")
 
   val Scala212 = "2.12.10"
-  val Scala213 = "2.13.0"
+  val Scala213 = "2.13.1"
   val ScalaVersions = Seq(Scala212, Scala213)
 
   val AkkaVersion = if (Nightly) "2.6.0" else "2.5.25"

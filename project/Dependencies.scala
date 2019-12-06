aa00cc13ea64ffe7625938afc02ad432a25eb6b7
Scala Steward <me@scala-steward.org>
2019-12-06 13:47:24 +0100

    Update mysql-connector-java to 8.0.18

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
     "com.typesafe.slick" %% "slick-hikaricp" % SlickVersion,
     "org.postgresql" % "postgresql" % "42.2.5" % Test,
     "com.h2database" % "h2" % "1.4.199" % Test,
-    "mysql" % "mysql-connector-java" % "8.0.15" % Test,
+    "mysql" % "mysql-connector-java" % "8.0.18" % Test,
     "com.microsoft.sqlserver" % "mssql-jdbc" % "7.2.1.jre8" % Test,
     "ch.qos.logback" % "logback-classic" % "1.2.3" % Test,
     "com.typesafe.akka" %% "akka-slf4j" % AkkaVersion % Test,

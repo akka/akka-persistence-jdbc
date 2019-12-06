49f5d67e883dfd2a0d1381412924e590eb2b4e64
Enno Runne <458526+ennru@users.noreply.github.com>
2019-12-06 13:43:26 +0100

    scalafmtSbt

--- project/AutomaticModuleName.scala
+++ project/AutomaticModuleName.scala
@@ -15,6 +15,6 @@ import sbt.{ Def, _ }
 object AutomaticModuleName {
   private val AutomaticModuleName = "Automatic-Module-Name"
 
-  def settings(name: String): Seq[Def.Setting[Task[Seq[PackageOption]]]] = Seq(
-    packageOptions in (Compile, packageBin) += Package.ManifestAttributes(AutomaticModuleName -> name))
+  def settings(name: String): Seq[Def.Setting[Task[Seq[PackageOption]]]] =
+    Seq(packageOptions in (Compile, packageBin) += Package.ManifestAttributes(AutomaticModuleName -> name))
 }

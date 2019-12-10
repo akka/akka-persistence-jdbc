d6f2f45548f407aa4e9c28bf9e9aabe6cd61e360
Enno Runne <458526+ennru@users.noreply.github.com>
2019-12-10 16:08:03 +0100

    Docs: update passphrase for rsync

--- .travis/travis-to-gustav.sh
+++ .travis/travis-to-gustav.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-# Decrypt private ssh key file
-# (File encryption and key/IV injection courtesy of Travis: https://docs.travis-ci.com/user/encrypting-files/)
-openssl aes-256-cbc -K $encrypted_9f19d2edcfdd_key -iv $encrypted_9f19d2edcfdd_iv -in .travis/travis_apjdbc_rsa.enc -out /tmp/id_rsa -d
 eval "$(ssh-agent -s)"
-chmod 600 /tmp/id_rsa
 # remove passphrase from private key
+cp .travis/deploy-key /tmp/id_rsa
+chmod 600 /tmp/id_rsa
 ssh-keygen -p -P "$RSYNC_PASSPHRASE" -N "" -f /tmp/id_rsa
 ssh-add /tmp/id_rsa
 sbt -jvm-opts .jvmopts-travis docs/publishRsync

ccdc29c7625b1969b243cc073b74cc870d533951
Kostya <samfrown@gmail.com>
2020-06-29 17:58:36 +0300

    Optimize query of highest seq number (#401)
    Co-authored-by: Konstantin.Samoylov <konstantin.samoylov@billing.ru>

--- core/src/main/scala/akka/persistence/jdbc/journal/dao/ByteArrayJournalDao.scala
+++ core/src/main/scala/akka/persistence/jdbc/journal/dao/ByteArrayJournalDao.scala
@@ -134,11 +134,11 @@ trait BaseByteArrayJournalDao extends JournalDaoWithUpdates with BaseJournalDaoW
   }
 
   private def highestMarkedSequenceNr(persistenceId: String) =
-    queries.highestMarkedSequenceNrForPersistenceId(persistenceId).result.headOption
+    queries.highestMarkedSequenceNrForPersistenceId(persistenceId).result
 
   override def highestSequenceNr(persistenceId: String, fromSequenceNr: Long): Future[Long] =
     for {
-      maybeHighestSeqNo <- db.run(queries.highestSequenceNrForPersistenceId(persistenceId).result.headOption)
+      maybeHighestSeqNo <- db.run(queries.highestSequenceNrForPersistenceId(persistenceId).result)
     } yield maybeHighestSeqNo.getOrElse(0L)
 
   override def messages(

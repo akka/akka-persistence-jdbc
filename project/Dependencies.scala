d7329cf5a576c73be1e59ccdbe838d13a50e299d
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-08-31 13:48:29 +0200

    mysql-connector-java 8.0.21 (was 8.0.20) (#424)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -18,7 +18,7 @@ object Dependencies {
   val JdbcDrivers = Seq(
     "org.postgresql" % "postgresql" % "42.2.16",
     "com.h2database" % "h2" % "1.4.200",
-    "mysql" % "mysql-connector-java" % "8.0.20",
+    "mysql" % "mysql-connector-java" % "8.0.21",
     "com.microsoft.sqlserver" % "mssql-jdbc" % "7.4.1.jre8")
 
   val Libraries: Seq[ModuleID] = Seq(

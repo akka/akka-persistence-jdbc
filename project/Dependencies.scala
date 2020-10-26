234bf73e39da77265c8f6d765f359565119a3b18
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2020-10-26 19:51:13 +0100

    postgresql 42.2.18 (was 42.2.16) (#447)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val ScalaTestVersion = "3.1.4"
 
   val JdbcDrivers = Seq(
-    "org.postgresql" % "postgresql" % "42.2.16",
+    "org.postgresql" % "postgresql" % "42.2.18",
     "com.h2database" % "h2" % "1.4.200",
     "mysql" % "mysql-connector-java" % "8.0.21",
     "com.microsoft.sqlserver" % "mssql-jdbc" % "7.4.1.jre8")

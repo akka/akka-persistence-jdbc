5266366af2e42d4a49fe7dec628679a0152fab7b
Enno Runne <458526+ennru@users.noreply.github.com>
2021-01-08 14:56:34 +0100

    Extend copyright to 2021

--- core/src/main/scala/akka/persistence/jdbc/serialization/PersistentReprSerializer.scala
+++ core/src/main/scala/akka/persistence/jdbc/serialization/PersistentReprSerializer.scala
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2014 - 2019 Dennis Vriend <https://github.com/dnvriend>
- * Copyright (C) 2019 - 2020 Lightbend Inc. <https://www.lightbend.com>
+ * Copyright (C) 2019 - 2021 Lightbend Inc. <https://www.lightbend.com>
  */
 
 package akka.persistence.jdbc.serialization

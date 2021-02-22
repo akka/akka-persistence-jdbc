1a4f8b2fb35301a417e29c1489a22a5fbf197954
Renato Cavalcanti <renato@cavalcanti.be>
2021-02-22 11:37:45 +0100

    Remove travis and fix credentials (#507)

--- .travis/travis-to-gustav.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-eval "$(ssh-agent -s)"
-# remove passphrase from private key
-cp .travis/deploy-key /tmp/id_rsa
-chmod 600 /tmp/id_rsa
-ssh-keygen -p -P "$RSYNC_PASSPHRASE" -N "" -f /tmp/id_rsa
-ssh-add /tmp/id_rsa
-sbt -jvm-opts .jvmopts-travis docs/publishRsync

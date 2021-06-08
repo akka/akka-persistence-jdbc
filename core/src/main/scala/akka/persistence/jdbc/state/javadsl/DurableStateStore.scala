936e53bc530dac6b944cabcf9ba04322e2e7257a
Debasish Ghosh <ghosh.debasish@gmail.com>
2021-06-08 17:25:20 +0530

    Refactored javadsl for DurableStateStore to reuse the Scala implementation

--- core/src/main/scala/akka/persistence/jdbc/state/javadsl/DurableStateStore.scala
+++ core/src/main/scala/akka/persistence/jdbc/state/javadsl/DurableStateStore.scala
@@ -3,53 +3,30 @@ package akka.persistence.jdbc.state.javadsl
 import java.util.Optional
 import java.util.concurrent.CompletionStage
 import scala.compat.java8.FutureConverters._
-import scala.concurrent.{ ExecutionContext, Future }
-import slick.jdbc.{ JdbcBackend, JdbcProfile }
+import scala.concurrent.ExecutionContext
+import slick.jdbc.JdbcProfile
 import akka.Done
 import akka.persistence.state.javadsl.{ DurableStateUpdateStore, GetObjectResult }
-import akka.serialization.Serialization
-import akka.persistence.jdbc.state.{ AkkaSerialization, DurableStateQueries }
+import akka.persistence.jdbc.state.DurableStateQueries
 import akka.persistence.jdbc.config.DurableStateTableConfiguration
-import akka.persistence.jdbc.state.DurableStateTables
-import akka.dispatch.ExecutionContexts
 
 class DurableStateStore[A](
-    db: JdbcBackend#Database,
     profile: JdbcProfile,
     durableStateConfigConfig: DurableStateTableConfiguration,
-    serialization: Serialization)(implicit ec: ExecutionContext)
+    scalaStore: akka.persistence.jdbc.state.scaladsl.DurableStateStore[A])(implicit ec: ExecutionContext)
     extends DurableStateUpdateStore[A] {
-  import profile.api._
 
   val queries = new DurableStateQueries(profile, durableStateConfigConfig)
 
   def getObject(persistenceId: String): CompletionStage[GetObjectResult[A]] =
-    toJava(db.run(queries._selectByPersistenceId(persistenceId).result).map { rows =>
-      rows.headOption match {
-        case Some(row) =>
-          GetObjectResult(
-            Optional
-              .ofNullable(AkkaSerialization.fromRow(serialization)(row).toOption.getOrElse(null))
-              .asInstanceOf[Optional[A]],
-            row.seqNumber)
-
-        case None =>
-          throw new Exception(s"State object creation failed during fetch from store: persistenceId $persistenceId")
-      }
-    })
-
-  def upsertObject(persistenceId: String, seqNr: Long, value: A, tag: String): CompletionStage[Done] = {
-    require(seqNr > 0)
-    val row =
-      AkkaSerialization.serialize(serialization, value).map { serialized =>
-        DurableStateTables
-          .DurableStateRow(persistenceId, serialized.payload, seqNr, serialized.serId, serialized.serManifest)
-      }
-
     toJava(
-      Future.fromTry(row).map(queries._upsertDurableState).flatMap(db.run).map(_ => Done)(ExecutionContexts.parasitic))
-  }
+      scalaStore
+        .getObject(persistenceId)
+        .map(x => GetObjectResult(Optional.ofNullable(x.value.getOrElse(null.asInstanceOf[A])), x.seqNr)))
+
+  def upsertObject(persistenceId: String, seqNr: Long, value: A, tag: String): CompletionStage[Done] =
+    toJava(scalaStore.upsertObject(persistenceId, seqNr, value, tag))
 
   def deleteObject(persistenceId: String): CompletionStage[Done] =
-    toJava(db.run(queries._delete(persistenceId)).map(_ => Done)(ExecutionContexts.parasitic))
+    toJava(scalaStore.deleteObject(persistenceId))
 }

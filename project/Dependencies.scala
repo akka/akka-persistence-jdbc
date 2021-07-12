f24ef44e4f4ec270abd3f6c68d34c99afe49fb39
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2021-07-12 18:41:25 +0200

    flyway-core 7.10.0 (was 7.5.4) (#546)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -33,7 +33,7 @@ object Dependencies {
     "org.scalatest" %% "scalatest" % ScalaTestVersion % Test) ++ JdbcDrivers.map(_ % Test)
 
   val Migration: Seq[ModuleID] = Seq(
-    "org.flywaydb" % "flyway-core" % "7.5.4",
+    "org.flywaydb" % "flyway-core" % "7.10.0",
     "com.typesafe" % "config" % "1.4.1",
     "ch.qos.logback" % "logback-classic" % "1.2.3",
     "org.testcontainers" % "postgresql" % "1.15.3" % Test,

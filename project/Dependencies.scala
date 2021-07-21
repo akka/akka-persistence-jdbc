131e7560299dd229e5b0b760372e120c05691074
Scala Steward <me@scala-steward.org>
2021-07-21 16:04:29 +0200

    postgresql 1.16.0 (was 1.15.3)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -36,6 +36,6 @@ object Dependencies {
     "org.flywaydb" % "flyway-core" % "7.10.0",
     "com.typesafe" % "config" % "1.4.1",
     "ch.qos.logback" % "logback-classic" % "1.2.3",
-    "org.testcontainers" % "postgresql" % "1.15.3" % Test,
+    "org.testcontainers" % "postgresql" % "1.16.0" % Test,
     "org.scalatest" %% "scalatest" % ScalaTestVersion % Test) ++ JdbcDrivers.map(_ % Provided)
 }

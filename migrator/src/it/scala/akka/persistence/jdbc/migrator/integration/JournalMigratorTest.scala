f67606d8d39730dbe4f30ec653edc5fd42cf631a
Gabriele Favaretto <gabfav@gmail.com>
2021-10-18 16:00:59 +0200

    wip

--- migrator/src/it/scala/akka/persistence/jdbc/migrator/integration/JournalMigratorTest.scala
+++ migrator/src/it/scala/akka/persistence/jdbc/migrator/integration/JournalMigratorTest.scala
@@ -1,19 +1,12 @@
 package akka.persistence.jdbc.migrator.integration
 
-import akka.persistence.jdbc.migrator.{JournalMigratorSpec, JournalMigratorTest}
+import akka.persistence.jdbc.migrator.JournalMigratorSpec._
+import akka.persistence.jdbc.migrator.JournalMigratorTest
 
-class PostgresJournalMigratorTest
-    extends JournalMigratorTest("postgres-application.conf")
-    with JournalMigratorSpec.PostgresCleaner
+class PostgresJournalMigratorTest extends JournalMigratorTest("postgres-application.conf") with PostgresCleaner
 
-class MySQLJournalMigratorTest
-    extends JournalMigratorTest("mysql-application.conf")
-    with JournalMigratorSpec.MysqlCleaner
+class MySQLJournalMigratorTest extends JournalMigratorTest("mysql-application.conf") with MysqlCleaner
 
-class OracleJournalMigratorTest
-    extends JournalMigratorTest("oracle-application.conf")
-    with JournalMigratorSpec.OracleCleaner
+class OracleJournalMigratorTest extends JournalMigratorTest("oracle-application.conf") with OracleCleaner
 
-class SqlServerJournalMigratorTest
-    extends JournalMigratorTest("sqlserver-application.conf")
-    with JournalMigratorSpec.SqlServerCleaner
+class SqlServerJournalMigratorTest extends JournalMigratorTest("sqlserver-application.conf") with SqlServerCleaner
--- migrator/src/test/scala/akka/persistence/jdbc/migrator/JournalMigratorTest.scala
+++ migrator/src/test/scala/akka/persistence/jdbc/migrator/JournalMigratorTest.scala
@@ -1,26 +1,29 @@
 package akka.persistence.jdbc.migrator
 
 import akka.pattern.ask
-import akka.persistence.jdbc.migrator.JournalMigratorSpec.{ AccountCreated, Deposit, Withdrawn }
+import akka.persistence.jdbc.migrator.JournalMigratorSpec._
 import akka.persistence.jdbc.query.ScalaJdbcReadJournalOperations
 
+/*
+  val journalConfig = new JournalConfig(config.getConfig("jdbc-journal"))
+  val snapshotConfig = new SnapshotConfig(config.getConfig("jdbc-snapshot-store"))
+  val readJournalConfig = new ReadJournalConfig(config.getConfig("jdbc-read-journal"))
+ */
 abstract class JournalMigratorTest(config: String) extends JournalMigratorSpec(config) {
 
   it should "work in progress" in {
     withActorSystem { implicit system =>
-      //val dao = new ByteArrayJournalDao(db, profile, journalConfig, SerializationExtension(system))
-
       val journalOps = new ScalaJdbcReadJournalOperations(system)
       withTestActors() { (actor1, actor2, actor3) =>
-        (actor1 ? AccountCreated(1)).futureValue //balance 1
-        (actor2 ? AccountCreated(2)).futureValue //balance 2
-        (actor3 ? AccountCreated(3)).futureValue //balance 3
+        (actor1 ? CreateAccount(1)).futureValue //balance 1
+        (actor2 ? CreateAccount(2)).futureValue //balance 2
+        (actor3 ? CreateAccount(3)).futureValue //balance 3
         (actor1 ? Deposit(3)).futureValue //balance 4
         (actor2 ? Deposit(2)).futureValue //balance 4
         (actor3 ? Deposit(1)).futureValue //balance 4
-        (actor1 ? Withdrawn(3)).futureValue //balance 3
-        (actor2 ? Withdrawn(3)).futureValue //balance 3
-        (actor3 ? Withdrawn(3)).futureValue //balance 3
+        (actor1 ? Withdraw(3)).futureValue //balance 3
+        (actor2 ? Withdraw(3)).futureValue //balance 3
+        (actor3 ? Withdraw(3)).futureValue //balance 3
 
         eventually {
           journalOps.countJournal.futureValue shouldBe 20
@@ -28,7 +31,10 @@ abstract class JournalMigratorTest(config: String) extends JournalMigratorSpec(c
 
         system.terminate()
       }
-      /* withActorSystem { implicit system =>
+    //val dao = new ByteArrayJournalDao(db, profile, journalConfig, SerializationExtension(system))
+
+    /**/
+    /* withActorSystem { implicit system =>
        val migrate = JournalMigrator(SlickDatabase.profile(cfg, "slick")).migrate()
          eventually {
            journalOps.countJournal.futureValue shouldBe 9

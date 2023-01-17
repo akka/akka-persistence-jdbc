e088c03cda7dbf8ba8a4228356593c2824dfa948
Scala Steward <scala_steward@virtuslab.com>
2023-01-17 13:20:38 +0000

    bump: scalatest 3.2.15 (was 3.2.13)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
   val AkkaBinaryVersion = AkkaVersion.take(3)
 
   val SlickVersion = "3.4.1"
-  val ScalaTestVersion = "3.2.13"
+  val ScalaTestVersion = "3.2.15"
 
   val JdbcDrivers = Seq(
     "org.postgresql" % "postgresql" % "42.3.8",

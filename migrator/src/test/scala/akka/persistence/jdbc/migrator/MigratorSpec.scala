31d6eb058bdfb66181ea531f4301a1df82f8671a
Johan Andrén <johan@markatta.com>
2023-02-21 10:32:28 +0100

    chore: Bump copyright year to 2023

--- migrator/src/test/scala/akka/persistence/jdbc/migrator/MigratorSpec.scala
+++ migrator/src/test/scala/akka/persistence/jdbc/migrator/MigratorSpec.scala
@@ -1,6 +1,6 @@
 /*
  * Copyright (C) 2014 - 2019 Dennis Vriend <https://github.com/dnvriend>
- * Copyright (C) 2019 - 2022 Lightbend Inc. <https://www.lightbend.com>
+ * Copyright (C) 2019 - 2023 Lightbend Inc. <https://www.lightbend.com>
  */
 
 package akka.persistence.jdbc.migrator

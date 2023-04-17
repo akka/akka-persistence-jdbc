23144672b7e3c5bf432e00f4a415d84683e2a449
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-04-17 12:40:11 +0200

    bump: postgresql 42.5.4 (was 42.5.3) (#729)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -12,7 +12,7 @@ object Dependencies {
   val ScalaTestVersion = "3.2.15"
 
   val JdbcDrivers = Seq(
-    "org.postgresql" % "postgresql" % "42.5.3",
+    "org.postgresql" % "postgresql" % "42.5.4",
     "com.h2database" % "h2" % "2.1.214",
     "mysql" % "mysql-connector-java" % "8.0.32",
     "com.microsoft.sqlserver" % "mssql-jdbc" % "7.4.1.jre8")

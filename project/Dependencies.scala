ec3a6855c69522345ee426c916fe79f1f5e08a8e
Scala Steward <scala_steward@virtuslab.com>
2023-06-24 12:22:59 +0000

    bump: scala-library 2.13.11 (was 2.13.10)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,7 +2,7 @@ import sbt._
 
 object Dependencies {
   val Scala212 = "2.12.17"
-  val Scala213 = "2.13.10"
+  val Scala213 = "2.13.11"
   val ScalaVersions = Seq(Scala213, Scala212)
 
   val AkkaVersion = "2.7.0"

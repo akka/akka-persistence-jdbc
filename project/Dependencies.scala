41e4718023f56607da69b918dc1b4adcc44c998f
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2023-10-04 15:39:18 +0200

    bump: postgresql 1.19.1 (was 1.19.0) (#772)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -32,6 +32,6 @@ object Dependencies {
   val Migration: Seq[ModuleID] = Seq(
     "com.typesafe" % "config" % "1.4.2",
     "ch.qos.logback" % "logback-classic" % "1.2.12",
-    "org.testcontainers" % "postgresql" % "1.19.0" % Test,
+    "org.testcontainers" % "postgresql" % "1.19.1" % Test,
     "org.scalatest" %% "scalatest" % ScalaTestVersion % Test) ++ JdbcDrivers.map(_ % Provided)
 }

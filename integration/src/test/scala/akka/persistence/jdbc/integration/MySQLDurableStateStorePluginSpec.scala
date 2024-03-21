423e206c41c7a98a5664f519f2c97ed09e62840d
Johan Andrén <johan@markatta.com>
2024-03-21 12:54:16 +0100

    revert: "Add MySQL implementation (#803)" (#807)
    This reverts commit 98710d8d6b105481b857cea2bbe86ed4d0792d52.

--- integration/src/test/scala/akka/persistence/jdbc/integration/MySQLDurableStateStorePluginSpec.scala
+++
@@ -1,8 +0,0 @@
-package akka.persistence.jdbc.integration
-
-import com.typesafe.config.ConfigFactory
-import slick.jdbc.MySQLProfile
-import akka.persistence.jdbc.state.scaladsl.DurableStateStorePluginSpec
-
-class MySQLDurableStateStorePluginSpec
-    extends DurableStateStorePluginSpec(ConfigFactory.load("mysql-shared-db-application.conf"), MySQLProfile) {}

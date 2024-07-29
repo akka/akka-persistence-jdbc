025d76cef9cd1f3488c9f7e8f0ae0878a3ba8e35
Scala Steward <scala_steward@virtuslab.com>
2024-07-29 09:12:04 +0000

    bump: mysql-connector-j 9.0.0 (was 8.4.0)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -16,7 +16,7 @@ object Dependencies {
   val JdbcDrivers = Seq(
     "org.postgresql" % "postgresql" % "42.7.3",
     "com.h2database" % "h2" % "2.2.224",
-    "com.mysql" % "mysql-connector-j" % "8.4.0",
+    "com.mysql" % "mysql-connector-j" % "9.0.0",
     "com.microsoft.sqlserver" % "mssql-jdbc" % "7.4.1.jre8")
 
   val Libraries: Seq[ModuleID] = Seq(

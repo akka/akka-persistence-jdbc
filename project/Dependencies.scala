6c84bcaa84dd4b06b94ddcab5f91edfa12a718f1
Sebastian <sebastian.alfers@gmail.com>
2024-09-20 07:06:10 +0200

    remove brackets from slick dependency

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -21,7 +21,7 @@ object Dependencies {
 
   val Libraries: Seq[ModuleID] = Seq(
     "com.typesafe.akka" %% "akka-persistence-query" % AkkaVersion,
-    ("com.typesafe.slick" %% "slick" % SlickVersion),
+    "com.typesafe.slick" %% "slick" % SlickVersion,
     "org.slf4j" % "slf4j-api" % "2.0.16",
     "com.typesafe.slick" %% "slick-hikaricp" % SlickVersion,
     "ch.qos.logback" % "logback-classic" % "1.5.7" % Test,

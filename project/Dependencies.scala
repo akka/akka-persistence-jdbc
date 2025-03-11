573df1b92b7c0f8edc8337918b5dc2c23291e440
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2025-03-11 15:46:27 +0100

    bump: logback-classic 1.5.17 (was 1.5.16) (#893)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -24,7 +24,7 @@ object Dependencies {
     "com.typesafe.slick" %% "slick" % SlickVersion,
     "org.slf4j" % "slf4j-api" % "2.0.17",
     "com.typesafe.slick" %% "slick-hikaricp" % SlickVersion,
-    "ch.qos.logback" % "logback-classic" % "1.5.16" % Test,
+    "ch.qos.logback" % "logback-classic" % "1.5.17" % Test,
     "com.typesafe.akka" %% "akka-slf4j" % AkkaVersion % Test,
     "com.typesafe.akka" %% "akka-persistence-tck" % AkkaVersion % Test,
     "com.typesafe.akka" %% "akka-stream-testkit" % AkkaVersion % Test,
@@ -33,7 +33,7 @@ object Dependencies {
 
   val Migration: Seq[ModuleID] = Seq(
     "com.typesafe" % "config" % "1.4.3",
-    "ch.qos.logback" % "logback-classic" % "1.5.16",
+    "ch.qos.logback" % "logback-classic" % "1.5.17",
     "org.testcontainers" % "postgresql" % "1.20.4" % Test,
     "org.scalatest" %% "scalatest" % ScalaTestVersion % Test) ++ JdbcDrivers.map(_ % Provided)
 }

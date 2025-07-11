6ad33205dbc35237d65928358d4ab856e8f4a6a4
Scala Steward <scala_steward@virtuslab.com>
2025-07-11 13:07:30 +0000

    bump: postgresql 1.21.3 (was 1.21.1)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -34,6 +34,6 @@ object Dependencies {
   val Migration: Seq[ModuleID] = Seq(
     "com.typesafe" % "config" % "1.4.3",
     "ch.qos.logback" % "logback-classic" % "1.5.18",
-    "org.testcontainers" % "postgresql" % "1.21.1" % Test,
+    "org.testcontainers" % "postgresql" % "1.21.3" % Test,
     "org.scalatest" %% "scalatest" % ScalaTestVersion % Test) ++ JdbcDrivers.map(_ % Provided)
 }
